abed9b7131c6a8894ef25fec94446ce5748bdbef
Rustam Ibragimov <iamdaiver@gmail.com>
2020-05-05 16:15:03 +0300

    [type-systems] ideas

--- lib/smart_core/initializer.rb
+++ lib/smart_core/initializer.rb
@@ -50,3 +50,26 @@ module SmartCore::Initializer
   type_alias(:module,  SmartCore::Types::Value::Module)
   type_alias(:any,     SmartCore::Types::Value::Any)
 end
+
+__END__
+
+include SmartCore::Initializer(type_system: :dry_types)
+
+SmartCore::Operation.configure do |config|
+  config.initializer.default_type_system = :smart_types
+end
+
+SmartCore::Initializer.configure(:initializer) do |config|
+  config.default_type_system = :smart_types
+end
+
+SmartCore::Initializer.plugin(:smart_types)
+SmartCore::Initializer.plugin(:thy)
+SmartCore::Initializer.plugin(:dry_types)
+
+SmartCore::Initializer.configure(:smart_types) do |config|
+  config.type_alias(:nil, SmartCore::Types::Value::Nil)
+end
+
+SmartCore::Initializer.configure(:thy) do |config|
+end
